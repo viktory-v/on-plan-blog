ce6e74984ad04e462f38b648b51e7143ce1adbe9
Viktoria Ocherentaya <viktoryviktor@gmail.com>
2021-10-02 00:19:00 +0300

    Add preview

--- src/woo/helpers/checkoutBlockClass.js
+++
@@ -1,9 +0,0 @@
-module.exports = function( blockIndex, step ) {
-  if ( step > blockIndex ) {
-    return 'block-done block-has-content';
-  } else if ( step === blockIndex ) {
-    return 'block-edited block-empty-content'
-  } else {
-    return 'block-empty block-empty-content'
-  }
-}
--- src/woo/helpers/priceFormat.js
+++
@@ -1,9 +0,0 @@
-// noinspection NpmUsedModulesInstalled
-const Handlebars = require( 'handlebars' );
-
-module.exports = function( price, currency ) {
-  currency = typeof currency === 'string' ? currency : '&#36;';
-  price = isNaN( price ) ? '0.00' : parseFloat( price ).toFixed( 2 );
-
-  return new Handlebars.SafeString( '<span class="woocommerce-Price-currencySymbol">' + currency + '</span>' + price );
-};
